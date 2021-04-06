44a7e8084846276a0ccc4669fd6641620e12141c
Xinhao Song <songxinhao@hotmail.com>
2021-04-06 00:26:49 -0500

    Create ApplicationConfig.java

---
+++ src/main/java/com/laioffer/SecondHandMarket/ApplicationConfig.java
@@ -0,0 +1,46 @@
+package com.laioffer.secondhandmarket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class ApplicationConfig {
+
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "BLn6ZX9c3af6qee";
+    private static final String RDS_INSTANCE = "laiproject-instance.cpqh9odayqyu.us-east-2.rds.amazonaws.com";
+
+    @Bean(name="entityManagerFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.laioffer.secondhandmarket.entity");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://" + RDS_INSTANCE +
+                ":3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC");
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        return dataSource;
+    }
+
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        return hibernateProperties;
+    }
+}
